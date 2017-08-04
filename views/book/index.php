d24b8f5a004971a787716f68677ecb59981d4181
Onibaku789 <varga.attila798@gmail.com>
2017-08-04 13:15:19 +0200

    hide id from the list

--- views/book/index.php
+++ views/book/index.php
@@ -24,7 +24,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
+          
             'name',
             'release_date',
             'author',
